2429457da5dfefc3348480cde376ac6175e4f405
vasiliev-a-v <78700149+vasiliev-a-v@users.noreply.github.com>
2021-05-03 17:57:53 +0300

    Delete take_electro/sql_install_db_code directory

--- take_electro/sql_install_db_code/1_make_user.sql
+++
@@ -1,7 +0,0 @@
---~ Создание пользователя и прав на psql
-ALTER ROLE "user" WITH LOGIN;
-ALTER ROLE "user" WITH CREATEDB;
-ALTER ROLE "user" WITH REPLICATION;
-
---~ Создание базы данных, владелец которой user
-CREATE DATABASE electro_temperature OWNER "user";
